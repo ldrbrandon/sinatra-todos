b33cf0887829ae2b8f420895a9c4e3548a9f5940
Apprentice <apprentice@dbc29.local>
2013-10-08 12:05:32 -0700

    Added create todo button

--- todo_app.rb
+++ todo_app.rb
@@ -5,13 +5,21 @@ require 'sinatra/activerecord'
 
 require 'models/todo'
 
-
+begin
 # This loads environment variables from the .env file
 require 'dotenv'
 Dotenv.load
+rescue LoadError
+end
+
 
 set :database, ENV['DATABASE_URL']
 
 get '/' do
   erb :index
+end
+
+post '/' do
+  Todo.create(name: params["todos"])
+  redirect '/'
 end
